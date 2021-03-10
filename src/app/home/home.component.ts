cbc82dec0f17442f31e1d7f29104deb5ad66a201
Lucas Gabriel <lucaslvr82@gmail.com>
2021-03-10 00:59:11 -0300

    add generator id, adding event icons
    Signed-off-by: Lucas Gabriel <lucaslvr82@gmail.com>

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,89 +12,153 @@ export class HomeComponent implements OnInit { /* classe  ue implementa uma inte
 
   ngOnInit(): void { /*inicializador da classe*/
 
-    this.alunos = "alunos da live itau"
+    this.alunos = "Cubo for Devs"
   }
 
   public alunos:string
-
   public pontos:number = 0
+  public result:string = ''
+
+
 
 
   public linhas:Item[][] = [
+    [  
+      {
+        ponto: 1,
+        tipo: "bandera",
+        id: this.generatorId()
+      },
+      {
+        ponto: 2,
+        tipo: "bandera",
+        id: this.generatorId()
+      },
+      {
+        ponto: 3,
+        tipo: "bandera",
+        id: this.generatorId()
+
+      },
+      {
+        ponto: 4,
+        tipo: "bandera",
+        id: this.generatorId()
+
+      },
+      {
+        ponto: 5,
+        tipo: "bomba",
+        id: this.generatorId()
+
+      },
+    ],
     [  {
         ponto: 1,
-        tipo: "bandera"
+        tipo: "bomba",
+        id: this.generatorId()
+
       },
       {
         ponto: 2,
-        tipo: "bandera"
+        tipo: "bandera",
+        id: this.generatorId()
+
       },
       {
         ponto: 3,
-        tipo: "bandera"
+        tipo: "bandera",
+        id: this.generatorId()
+
       },
       {
         ponto: 4,
-        tipo: "bandera"
+        tipo: "bandera",
+        id: this.generatorId()
+
       },
       {
         ponto: 5,
-        tipo: "bomba"
+        tipo: "bandera",
+        id: this.generatorId()
+
       },
     ],
-        [  {
+    [  {
         ponto: 1,
-        tipo: "bomba"
+        tipo: "bandera",
+        id: this.generatorId()
+
       },
       {
         ponto: 2,
-        tipo: "bandera"
+        tipo: "bandera",
+        id: this.generatorId()
+
       },
       {
         ponto: 3,
-        tipo: "bandera"
+        tipo: "bomba",
+        id: this.generatorId()
+
       },
       {
         ponto: 4,
-        tipo: "bandera"
+        tipo: "bandera",
+        id: this.generatorId()
+
       },
       {
         ponto: 5,
-        tipo: "bandera"
-      },
-    ],
-        [  {
-        ponto: 1,
-        tipo: "bandera"
-      },
-      {
-        ponto: 2,
-        tipo: "bandera"
-      },
-      {
-        ponto: 3,
-        tipo: "bomba"
-      },
-      {
-        ponto: 4,
-        tipo: "bandera"
-      },
-      {
-        ponto: 5,
-        tipo: "bandera"
+        tipo: "bandera",
+        id: this.generatorId()
+
       },
     ]
   ] 
 
 
   bombaOubandeira(item:Item) {
+    
+    
+    this.verifyIcon(item)
+
+
     if(item.tipo == 'bomba') {
       this.pontos = 0
-      alert("opaaaa a bomba estourou e vc foi eliminado")
+      /*alert("opaaaa a bomba estourou e vc foi eliminado")*/
+      this.result = "opaaaa a bomba estourou e você foi eliminado"
       return
     }
 
     this.pontos += 1
+    this.result = "Você tem " + this.pontos + " pontos"
   }
+
+  
+
+
+  verifyIcon(item:Item){
+
+    let icon = 'button.' + item.id
+    const divIcon = document.querySelector(icon)
+
+    if(item.tipo == 'bomba') {
+      divIcon.innerHTML = '<i class="fas fa-bomb"></i>'
+    }
+    else {
+      divIcon.innerHTML = `<i class="fas fa-flag"></i>`;
+    }
+
+  }
+
+
+
+  generatorId () {
+    return '_' + Math.random().toString(36).substr(2, 9);
+  }
+
+
+
 
 }
